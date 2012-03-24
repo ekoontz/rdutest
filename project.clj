1b58e898ec94fe317cdf4489c4f1679d6e2af153
Eugene Koontz <ekoontz@hiro-tan.org>
2012-03-23 20:26:47 -0700

    add dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,8 @@
 (defproject rdutest "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [slice "0.4.0-SNAPSHOT"]])
+
+
+
